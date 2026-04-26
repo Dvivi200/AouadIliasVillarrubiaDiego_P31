f8a855ffea26496d12498ed61a611eabadce7e37
Dvivi200 <diegovillarrubia2007@gmail.com>
2026-04-26 11:44:33 +0200

    Afegit

---
+++ src/prog2/adaptador/Adaptador.java
@@ -0,0 +1,19 @@
+package prog2.adaptador;
+
+import prog2.model.Dades;
+import prog2.vista.BiblioException;
+
+import java.io.Serializable;
+
+public class Adaptador implements Serializable {
+
+    private Dades dades;
+
+    public void guardaDades(String camiDesti) throws BiblioException {
+
+    }
+    public void carregaDades(String camiOrigen) throws BiblioException {
+
+    }
+
+}
