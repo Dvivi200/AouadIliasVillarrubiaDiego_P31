abb44f39b268d2881fba56a734e0a89171ee0542
Iliassss19 <iaouadsa7@alumnes.ub.edu>
2026-05-04 23:31:07 +0200

    iniciador biblio

--- src/prog2/vista/IniciadorBiblioUB.java
+++ src/prog2/vista/IniciadorBiblioUB.java
@@ -1,4 +1,8 @@
 package prog2.vista;
 
 public class IniciadorBiblioUB {
+    public static void main(String[]args){
+        BiblioUB biblioUb = new BiblioUB();
+        biblioUb.gestioBiblioUB();
+    }
 }
